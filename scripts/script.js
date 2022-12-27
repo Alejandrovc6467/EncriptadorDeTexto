d460c137a9951fdad4b9f20a5b3babb666e23ac0
Alejandrovc6467 <alejandrovc6467@gmail.com>
2022-12-26 23:30:39 -0600

    casiTerminado

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,69 @@
 
 
-function encriptar(){
-
-    alert("sirve");
+function getFocus() {
+    let textArea = document.getElementById("textAreaIzq");
+    textArea.focus();
 }
 
-/**rotar " Solo letras minúsculas y sin acentos" cuando no cumpla */
+getFocus();//lo llamo aqui para que inicie con el focus al cargar
 
 
-function encriptar2() {
+function copy() {
+    let texto = document.getElementById("textAreaDer");
+    texto.select();
+    document.execCommand("copy");
+    Swal.fire({
+        icon: 'success',
+        iconColor: '#b9ab9c',
+        background: '#ab5d89',
+        title: '¡Bien!',
+        confirmButtonColor: '#b9ab9c',
+        text: 'Texto copiado correctamente',
+    });
+}
+
+
+function encriptar() {
+
+
     let sinMensaje = document.getElementById("sinMensaje");
-
-    sinMensaje.style.display= "none";
-
-
     let salidaDetexto = document.getElementById("salidaDetexto");
-    salidaDetexto.style.display= "flex";
-
     let contenedorBotonesDerecho = document.getElementById("contenedorBotonesDerecho");
-    contenedorBotonesDerecho.style.display= "flex";
-
-}
+    let texto = document.getElementById("textAreaIzq").value;
 
 
+    if (texto.length!=0) {
 
-function encriptar3() {
-    let texto = document.getElementById("input-texto").value;
-    let txt_cifrado = texto.replace(/e/gm, "enter");
-    txt_cifrado = txt_cifrado.replace(/o/gm, "ober");
-    txt_cifrado = txt_cifrado.replace(/i/gm, "imes");
-    txt_cifrado = txt_cifrado.replace(/a/gm, "ai");
-    txt_cifrado = txt_cifrado.replace(/u/gm, "ufat");
+        if (/[^a-zñ ]/.test(texto)) {
+            alert("Hacer que se mueva la advertencia o no se modificar esta ventana");
+            Swal.fire({
+                icon: 'error',
+                iconColor: '#b9ab9c',
+                background: '#E3E0DE',
+                title: 'Oops...',
+                confirmButtonColor: '#b9ab9c',
+                text: 'Solo se permiten letras minusculas y sin acento',
+              });
 
-    if (/[^a-zñ ]/.test(texto)) {
-        Swal.fire({
-            icon: 'error',
-            iconColor: '#b9ab9c',
-            background: '#E3E0DE',
-            title: 'Oops...',
-            confirmButtonColor: '#b9ab9c',
-            text: 'Solo se permiten letras minusculas y sin acento',
-          });
-    }
-    else if (texto.length === 0) {
+        }else{
+
+            sinMensaje.style.display= "none";
+            salidaDetexto.style.display= "flex";
+            contenedorBotonesDerecho.style.display= "flex";
+
+            let cifradoText = texto.replace(/e/gm, "enter");
+            cifradoText = cifradoText.replace(/i/gm, "imes");
+            cifradoText = cifradoText.replace(/a/gm, "ai");
+            cifradoText = cifradoText.replace(/o/gm, "ober");
+            cifradoText = cifradoText.replace(/u/gm, "ufat");
+
+            document.getElementById("textAreaDer").innerHTML = cifradoText;
+
+        }
+ 
+
+    }else{
+        alert("modficar la venta de vacio o hacer otra cosa no se");
         Swal.fire({
             icon: 'error',
             iconColor: '#b9ab9c',
@@ -50,19 +71,28 @@ function encriptar3() {
             title: 'Oops...',
             confirmButtonColor: '#b9ab9c',
             text: 'El campo de texto está vacio, escriba el texto que desea encriptar',
+            width: '250px',
           });
     }
-    else {
-        document.getElementById("texto1-contder").style.display = "none";
-        document.getElementById("texto2-contder").style.display = "none";
-        document.getElementById("output-texto").style.display = "inline-block";
-        document.getElementById("output-texto").innerHTML = txt_cifrado;
-        
-        value();
-    }
 
-    
 }
+
+
+function desencriptar(){
+
+
+
+
+alert("desencriptar voy por esto");
+
+
+}
+
+
+alert("Revisar que se desencripte bien el texto prueba del TRELLO y compararlo con m");
+
+
+
 
 
 
